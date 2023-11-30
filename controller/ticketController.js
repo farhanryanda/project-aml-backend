f06e93c25956446a59f5d9e82789e7076c2b4970
farhanryanda <coegstory@gmail.com>
2023-11-30 09:47:14 +0700

    updateTicket

--- controller/ticketController.js
+++ controller/ticketController.js
@@ -121,7 +121,7 @@ const updateTicket = async (res, req) => {
     );
     console.log(numRowsUpdated);
     if (numRowsUpdated === 1) {
-      const updatedTicket = await Ticket.findOne({ where: { id } });
+      const updatedTicket = await Ticket.findByPk({ where: { id } });
       if (updatedTicket) {
         res.status(200).json({
           status: "OK",
