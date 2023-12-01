586c0d8b7f8211af8ad51666f6ef6f53fdadcad4
farhanryanda <coegstory@gmail.com>
2023-12-01 10:49:05 +0700

    cors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const ticketRouter = require("./ticketRouter");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 router.use("/api/v1", ticketRouter);
 
 module.exports = router;
